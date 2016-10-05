a1ea54845c2c2c0108ccfa4a45fc0954a597396d
Nathan Boxhall-Burnett <nboxhallburnett@appcelerator.com>
2016-10-05 12:43:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Fibonacci-cli
+======
+
+Prints out the fibonacci number at the index provided
+
+### Usage
+
+```
+> fibonacci [index]
+```
+
+Example:
+```
+> fibonacci 10
+89
+```
---
+++ cli.js
@@ -0,0 +1,21 @@
+#! /usr/local/bin/node
+'use strict';
+
+const help = "Usage: fibonacci [index]"
+
+// We need numbers
+if (isNaN(+process.argv[2])) {
+	console.log(help);
+	process.exit(1);
+}
+
+// Calculate a fibonacci number
+function fibCalc (total, prev = 1, current = 1) {
+	if (total <= 1) {
+		return current;
+	}
+	return fibCalc(--total, current, prev + current);
+}
+
+// Log the fibonacci number
+console.log(process.argv[2] ? fibCalc(+process.argv[2]) : help);
